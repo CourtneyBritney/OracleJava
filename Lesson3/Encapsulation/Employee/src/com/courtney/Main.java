0cd46baccc7f278187b039babf92cd0b583d597b
CourtneyBritney <courtney02alexander@gmail.com>
2021-01-14 13:37:07 +0200

    Encapsulation and Subclassing

---
+++ Lesson3/Encapsulation/Employee/src/com/courtney/Main.java
@@ -0,0 +1,10 @@
+package com.courtney;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Manager mgr = new Manager(102, "Courtney Alexander", "107-99-9078", 20678.50, "Software development");
+	    mgr.raiseSalary(30987.48);
+	    //String dept = mgr.getDeptName();
+    }
+}
